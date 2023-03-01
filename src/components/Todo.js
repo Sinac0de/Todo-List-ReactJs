c8494324c8fc68090edeeff2440d5fcd24aed750
Sina Moradian <sinacsharp@gmail.com>
2023-03-02 01:51:52 +0330

    Add line through to completed todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,9 @@
 const Todo = ({ todo, onComplete }) => {
   return (
     <div className="flex w-96 bg-slate-500 items-center justify-between m-2 p-2">
-      <p>{todo.text}</p>
+      <p className={todo.isCompleted ? "line-through opacity-60" : ""}>
+        {todo.text}
+      </p>
       <div className="flex gap-4">
         <button>Edit</button>
         <button onClick={onComplete}>Completed</button>
