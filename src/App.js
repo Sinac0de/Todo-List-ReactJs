61c9aff6c313c97e3effae6199b7395170d064ca
Sina Moradian <sinacsharp@gmail.com>
2023-03-03 18:15:00 +0330

    change header Styles

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import TodoApp from "./components/TodoApp";
 const App = () => {
   return (
     <div className="flex flex-col items-center">
-      <h1 className="m-2 text-2xl">TodoList App</h1>
+      <h1 className="m-2 text-5xl">TodoList App</h1>
       <TodoApp />
     </div>
   );
