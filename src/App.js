bf97845e3f4880c9516457f7cbe05a0028da9e81
Sina Moradian <sinacsharp@gmail.com>
2023-03-03 22:57:26 +0330

    Add font-family and style the header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import TodoApp from "./components/TodoApp";
 const App = () => {
   return (
     <div className="flex flex-col items-center">
-      <h1 className="m-2 text-5xl">TodoList App</h1>
+      <h1 className="mx-2 mt-8 mb-3 font-Oleo text-5xl text-transparent bg-gradient-to-r from-indigo-500 to-blue-500 bg-clip-text">
+        TodoList App
+      </h1>
       <TodoApp />
     </div>
   );
