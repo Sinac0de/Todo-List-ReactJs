a794a7e7ad37c48596588cf111d1c1cf81c82e41
Sina Moradian <sinacsharp@gmail.com>
2023-03-11 02:31:15 +0330

    Add filter component without functionality

---
+++ src/components/FilterTodos.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+import Select from "react-select";
+
+const options = [
+  { value: "all", label: "All" },
+  { value: "completed", label: "Completed" },
+  { value: "uncompleted", label: "UnCompleted" },
+];
+
+const FilterTodos = ({ selectedOption, onChange }) => {
+  return (
+    <Select
+      placeholder={"Select a filter..."}
+      value={selectedOption}
+      onChange={onChange}
+      options={options}
+      className={"mt-2"}
+    />
+  );
+};
+
+export default FilterTodos;
