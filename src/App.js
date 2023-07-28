60dc5362a3b9f03839cc7f543d7b224abdaf5c1b
Sina Moradian <sinacsharp@gmail.com>
2023-07-28 07:09:29 +0330

    Fix the color of the header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import TodoApp from "./components/TodoApp";
 const App = () => {
   return (
     <div className="flex flex-col items-center">
-      <h1 className="mx-2 mt-8 mb-3 font-Oleo text-5xl text-transparent bg-gradient-to-r from-indigo-500 to-blue-500 bg-clip-text">
+      <h1 className="mx-2 mt-8 mb-3 font-Oleo text-5xl text-white">
         TodoList App
       </h1>
       <TodoApp />
